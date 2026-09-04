6aa892775fe65c8b0befc5d466f91cc4444133a4
Lesedi Phalatse <Lesediphaltse13@gmail.com>
2026-09-04 16:29:47 +0200

    Uncomment database and table creation SQL statements

--- ST10447256_LesediPhalatse_Part1/RaceDay-Database.sql
+++ ST10447256_LesediPhalatse_Part1/RaceDay-Database.sql
@@ -7,3 +7,17 @@ CREATE TABLE UserRole
     RoleId INT IDENTITY(1,1) PRIMARY KEY,
     RoleName VARCHAR(30) NOT NULL UNIQUE
 );
+
+CREATE TABLE Organizers
+(
+    OrganiserId INT IDENTITY(1,1) PRIMARY KEY,
+    RoleId INT NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    PasswordHash VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(20),
+    CreatedAt DATETIME2 DEFAULT GETDATE(),
+
+    FOREIGN KEY (RoleId) REFERENCES UserRole(RoleId)
+);
